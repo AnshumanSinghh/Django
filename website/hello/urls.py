b82b90a739825a8c05ad0914ba269116c5d6e137
AnshumanSinghh <2801anshuman@gmail.com>
2021-01-28 00:53:58 +0530

    Added two apps (hello, newyear)

--- website/hello/urls.py
+++ website/hello/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("anshuman", views.anshuman, name="anshuman"),
+    path("python", views.python, name="python"),
+    path("<str:name>", views.greet, name="greet"),
 ]
--- website/hello/views.py
+++ website/hello/views.py
@@ -4,5 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    return render(request, "hello/index.html") # returning html file
     
+def anshuman(request):
+    return HttpResponse("<h1>Hello, Anshuman Singh!!<h1>")
+
+def python(request):
+    return HttpResponse("<strong style='font-size:50px;'> I'm using Python!! </strong>") # returning HttpResponse only.
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize(),
+    }) # render takes three arguments: request, "template name with route", context <dictionary of variables>
